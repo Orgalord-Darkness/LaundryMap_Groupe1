ee6f0714f3c83bc312d9eca679eb2c5fd7b4426d
Qwezen <46681515+Qwezen@users.noreply.github.com>
2026-03-14 19:08:06 +0100

    S1 - Front - Inscription et Dashboard
    S1 - Front - Inscription et Dashboard Pro & admin :
    
    - Inscription
    - Dashboard
    - + Routes & composants cards (en cours).

---
+++ laundrymap-front/src/pages/admin/dashboard.tsx
@@ -0,0 +1,60 @@
+// import { useState } from 'react';
+import { Link } from 'react-router';
+
+function AdminDashboard() {
+  const stats = {
+    laundries: 72,
+    users: 109,
+    accountsToValidate: 17,
+    pendingLaundries: 4,
+    reportedComments: 23,
+  };
+
+  return (
+    <div className="min-h-screen bg-gray-50 p-4 md:p-8">
+      <h1 className="font-bold text-2xl mt-6 text-center">Tableau de bord</h1>
+      <p className="text-gray-500 text-center mt-2"> Bienvenue dans votre espace administratif </p>
+
+      {/* Cards stats */}
+      <div className="grid grid-cols-2 md:grid-cols-2 md:mx-auto md:w-[500px] gap-4 mt-18">
+        <Link to="/laveries" className="bg-[#0077B6] text-white p-4 rounded-lg shadow-md text-center cursor-pointer hover:bg-blue-600 transition-colors block" >
+          <p className="text-lg md:text-xl font-semibold">{stats.laundries}</p>
+          <p className="text-sm md:text-lg">Laveries</p>
+        </Link>
+        <Link to="/utilisateurs" className="bg-[#0077B6] text-white p-4 rounded-lg shadow-md text-center cursor-pointer hover:bg-blue-600 transition-colors block" >
+          <p className="text-lg md:text-xl font-semibold">{stats.users}</p>
+          <p className="text-sm md:text-lg">Utilisateurs</p>
+        </Link>
+      </div>
+
+      {/* Sections grises */}
+      <div className="mt-6 grid grid-cols-1 md:grid-cols-3 gap-4">
+        <Link to="/comptes-a-valider" className="bg-gray-800 text-white p-6 rounded-lg shadow-md h-32 flex items-center justify-between cursor-pointer hover:bg-gray-700 transition-colors block" >
+          <div>
+            <p className="font-semibold">Comptes à valider</p>
+            <p className="text-sm text-gray-300"> Dernière demande il y a 48 min </p>
+          </div>
+          <p className="text-4xl font-bold">{stats.accountsToValidate}</p>
+        </Link>
+
+        <Link to="/laveries-en-attente" className="bg-gray-800 text-white p-6 rounded-lg shadow-md h-32 flex items-center justify-between cursor-pointer hover:bg-gray-700 transition-colors block" >
+          <div>
+            <p className="font-semibold">Laveries en attentes</p>
+            <p className="text-sm text-gray-300"> Dernière demande il y a 20 min </p>
+          </div>
+          <p className="text-4xl font-bold">{stats.pendingLaundries}</p>
+        </Link>
+
+        <Link to="/commentaires-signales" className="bg-gray-800 text-white p-6 rounded-lg shadow-md h-32 flex items-center justify-between cursor-pointer hover:bg-gray-700 transition-colors block" >
+          <div>
+            <p className="font-semibold">Commentaires signalés</p>
+            <p className="text-sm text-gray-300"> Dernier signalement il y a 5 min</p>
+          </div>
+          <p className="text-4xl font-bold">{stats.reportedComments}</p>
+        </Link>
+      </div>
+    </div>
+  );
+}
+
+export default AdminDashboard;
---
+++ laundrymap-front/src/pages/admin/login.tsx
@@ -0,0 +1,88 @@
+import { useState } from 'react';
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Field, FieldDescription, FieldLabel } from "@/components/ui/field";
+
+
+function AdminLogin() {
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [errors, setErrors] = useState({
+    email: "",
+    password: "",
+  });
+
+ // validation formulaire
+  const validateForm = () => {
+  
+    const newErrors = { email: "", password: "" };
+
+    if (!email) {
+      newErrors.email = "L'email est requis";
+    } else if (!email.includes("@") || !email.includes(".")) {
+      newErrors.email = "Email invalide";
+    }
+
+    if (!password) {
+      newErrors.password = "Le mot de passe est requis";
+    } else if (password.length < 8) {
+      newErrors.password = "Le mot de passe doit contenir au moins 8 caractères";
+    }
+
+    setErrors(newErrors);
+    return !newErrors.email && !newErrors.password;
+  };
+
+  // Fonction pour gérer la soumission du formulaire
+  const handleSubmit = (event: React.SubmitEvent<HTMLFormElement>) => {
+    event.preventDefault(); // Empêche le rechargement de la page
+
+    if (validateForm()) {
+
+      fetch("/api/admin/login", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          email: email,
+          password: password,
+        }),
+      })
+      .then((response) => response.json())
+ 
+    }
+  };
+
+  return (
+    <>
+
+    <form onSubmit={handleSubmit} className="flex flex-col items-center p-4">
+
+      <h1 className="font-bold text-2xl mt-6">Connexion</h1>
+      <p className="text-gray-500 text-center mt-2">Se connecter en tant que administrateur</p>
+
+      <Field className="w-11/12 max-w-md mt-16">
+        <FieldLabel htmlFor="email">Email <span className="text-orange-600">*</span></FieldLabel>
+        <Input id="email" type="email" placeholder="votre@email.com" value={email} onChange={(event) => setEmail(event.target.value)} className="h-11"/>
+        {errors.email && <p className="text-red-500 text-sm mt-1">{errors.email}</p>}
+      </Field>
+
+      <Field className="w-11/12 max-w-md mt-4">
+        <FieldLabel htmlFor="password">Mot de passe <span className="text-orange-600">*</span></FieldLabel>
+        <Input id="password" type="password" placeholder="••••••••" value={password} onChange={(event) => setPassword(event.target.value)} className="h-11" />
+        {errors.password && <p className="text-red-500 text-sm mt-1">{errors.password}</p>}
+      </Field>
+
+      <div className="w-11/12 max-w-md mt-8">
+        <Button type="submit" className="w-full">Connexion</Button>
+      </div>
+
+    </form>
+
+    </>
+  )
+}
+
+export default AdminLogin;
