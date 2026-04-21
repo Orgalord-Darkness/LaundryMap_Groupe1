cbda8ee508f4b7d1f1d82dd5c0558fccf6156bae
Orgalord_Darkness <heddy.mameri@gmail.com>
2026-04-21 18:43:54 +0200

    [130] - Ajout consigne google compte login user + ajout trads manquantes

--- laundrymap-front/src/pages/user/login.tsx
+++ laundrymap-front/src/pages/user/login.tsx
@@ -109,7 +109,7 @@ export default function Connexion() {
             </p>
 
             <a href="/pro/login" className="text-center text-sm text-gray-700 underline font-medium cursor-pointer" aria-label="Se connecter en tant que professionnel">
-                Se connecter en tant que professionnel ?
+               {t('consigne_co_pro')}
             </a>
 
             {successMessage && (
@@ -152,17 +152,18 @@ export default function Connexion() {
 
             <div className="flex justify-end">
                 <a href="/user/mot-de-passe-oublie" className="text-sm text-gray-700 underline underline-offset-2 hover:text-[#1e90d6] transition-colors" aria-label="Mot de passe oublié">
-                    Mot de passe oublié ?
+                    {t('mdp_oublie')}
                 </a>
             </div>
 
             <a href='/user/inscription' className="text-center text-sm text-gray-700 underline font-medium cursor-pointer" aria-label="S'inscrire en tant qu'utilisateur">
-                Pas de compte ? Cliquer ici pour s'inscrire
+                {t('lien_inscription')}
             </a>
 
             <Button type="submit" className="mt-2 w-full h-12 rounded-xl" aria-label='Se connecter'>
-                Se connecter
+                {t('connexion')}
             </Button>
+            <p className="text-center text-sm text-gray-600 mt-4">{t('consigne_co_google')}</p>
             <GoogleLoginButton
                 route={`${import.meta.env.VITE_API_BASE_URL}/api/v1/utilisateur/inscription/google`}
                 title='Se connecter avec Google'
